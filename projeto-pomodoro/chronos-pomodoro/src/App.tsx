2283e9f57dadefcb04b8b2379cae86f5c3107b2d
ystxrm <arthuroliveira2815@gmail.com>
2026-03-26 20:37:25 -0300

    pratica04

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,15 +1,9 @@
-// src/App.jsx
-
-// 1º: Importamos as variáveis (o tema)
-import './styles/theme.css';
-// 2º: Importamos os estilos globais
-import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <h1>Olá, Mundo!</h1>
-      <p>Testando o CSS Global.</p>
+      <Heading />
     </>
   );
 }
