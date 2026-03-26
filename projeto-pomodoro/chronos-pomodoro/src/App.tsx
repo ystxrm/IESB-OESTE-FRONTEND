64cb924dbf6f7892e750c2857e7fff53d6c5fc4b
ystxrm <arthuroliveira2815@gmail.com>
2026-03-26 20:58:45 -0300

    pratica08

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,14 +1,35 @@
-// src/App.tsx
-import { Heading } from './components/Heading';
-import { Timer } from 'lucide-react'; // Importando o ícone
+// src/App.jsx
 
 export function App() {
   return (
-    <Heading>
-      Histórico
-      <button>
-        <Timer /> {/* Renderizando o ícone dentro do botão */}
-      </button>
-    </Heading>
+    <>
+      {/* Seção 1: Logo */}
+      <div className='container'>
+        <div className='content'>
+          <p>Logo do App</p>
+        </div>
+      </div>
+
+      {/* Seção 2: Menu */}
+      <div className='container'>
+        <div className='content'>
+          <p>Menu de Navegação</p>
+        </div>
+      </div>
+
+      {/* Seção 3: Formulário / Cronômetro */}
+      <div className='container'>
+        <div className='content'>
+          <p>Área do Cronômetro</p>
+        </div>
+      </div>
+
+      {/* Seção 4: Footer */}
+      <div className='container'>
+        <div className='content'>
+          <p>Rodapé da Página</p>
+        </div>
+      </div>
+    </>
   );
 }
