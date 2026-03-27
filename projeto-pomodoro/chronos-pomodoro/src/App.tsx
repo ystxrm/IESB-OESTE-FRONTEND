4e0c6092dc3c6bb1cf06eb4d85c42030cbd7ea6e
ystxrm <arthuroliveira2815@gmail.com>
2026-03-26 21:29:58 -0300

    pratica14

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,10 +1,10 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -12,11 +12,40 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
       </Container>
+
       <Container>
         <CountDown />
+      </Container>
+
+      {/* Nossa nova estrutura de formulário */}
+      <Container>
+        <form className='form' action=''>
+          {/* Grupo 1: Label e Input */}
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          {/* Grupo 2: Texto de apoio */}
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          {/* Grupo 3: Ciclos */}
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          {/* Grupo 4: Botão */}
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
