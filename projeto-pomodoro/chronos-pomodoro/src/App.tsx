b30e4271c06f2de2d6017060e30e791fd7e768e3
ystxrm <arthuroliveira2815@gmail.com>
2026-03-26 21:27:13 -0300

    pratica12

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,6 +1,6 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -13,7 +13,7 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
